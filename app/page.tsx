a4fc35d52697b4908895fd7a18542a47b0954875
Pmthk1 <66022635@up.ac.th>
2024-11-28 22:45:10 +0700

    231sa

--- app/page.tsx
+++ app/page.tsx
@@ -18,17 +18,17 @@ export default function Home() {
     >
       <div className="text-center">
         {/* Welcome Message */}
-        <h3 className="mb-4 text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        <h3 className="mb-4 text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-gray-200 to-gray-400">
           Welcome to My Portfolio
         </h3>
-        <h3 className="mb-8 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        <h3 className="mb-8 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-gray-300 to-gray-500">
           Thanakrit Suphamongkhon
         </h3>
         
         {/* Button */}
         <button
           onClick={handleClick}
-          className="px-10 py-4 text-lg font-medium text-white bg-gradient-to-r from-green-400 to-blue-500 rounded-full shadow-lg hover:from-green-500 hover:to-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300"
+          className="px-10 py-4 text-lg font-medium text-gray-100 bg-gradient-to-r from-gray-600 to-gray-800 rounded-full shadow-md hover:from-gray-500 hover:to-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300"
         >
           View My Portfolio
         </button>
