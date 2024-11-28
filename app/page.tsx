464e1342ea09f7b18fd1868c5610343a6f00e92a
Pmthk1 <66022635@up.ac.th>
2024-11-28 22:07:10 +0700

    wqewqe2

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           onClick={handleClick}
           className="px-6 py-3 text-white bg-blue-500 rounded-lg shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
         >
-          NextPage
+          Next Page
         </button>
       </div>
     </div>
