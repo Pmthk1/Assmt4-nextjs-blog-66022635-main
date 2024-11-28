4221108bd56941691cfb70891e4f24abb5341620
Pmthk1 <66022635@up.ac.th>
2024-11-28 22:21:48 +0700

    asdase213

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,23 @@ export default function Home() {
   const router = useRouter();
 
   const handleClick = () => {
-    router.push("/data"); 
+    router.push("/data");
   };
 
   return (
     <div
-      className="relative min-h-screen bg-cover bg-center"
+      className="flex items-center justify-center min-h-screen bg-cover bg-center"
       style={{
         backgroundImage: "url('/image/background 1.jpg')",
       }}
     >
-      <div className="flex items-center justify-center">
-        <h3 className=" text-xl font-semibold bg-slate-50">
+      <div className="text-center">
+        <h3 className="mb-6 text-2xl font-bold text-white bg-black bg-opacity-50 rounded-md px-4 py-2">
           Welcome to Portfolio Thanakrit Suphamongkhon
         </h3>
-      </div>
-      <div className="flex items-center justify-center h-full">
         <button
           onClick={handleClick}
-          className="px-6 py-3 text-white bg-blue-500 rounded-lg shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+          className="px-8 py-4 text-lg font-medium text-white bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
         >
           Next Page
         </button>
